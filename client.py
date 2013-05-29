dfea623125da2082fae7028556d25d298340909b
VGMoose <vgmoose@gmail.com>
2013-05-28 22:08:35 -0400

    simplified client code and added msg for future support. Also change the way messages are received in a shell, and fixed /shell command

--- client.py
+++ client.py
@@ -1,7 +1,7 @@
 from socket import *
 from thread import *
 from sys import *
-import struct,fcntl,termios, readline, os
+import readline, os, time,tty
 
 PINK = '\033[95m'
 BLUE = '\033[94m'
@@ -9,45 +9,49 @@ GREEN = '\033[92m'
 YELLOW = '\033[93m'
 RED = '\033[91m'
 ENDC = '\033[0m'
+BLINK = '\033[5m'
+
 my_name = 'User'
 
 loop = True
-clients = 2
+shell_active = False
+clients = 1
 
-if platform == 'win32':
-    my_name = os.getenv('USERNAME')
-else:
-    my_name = os.getenv('USER')
+my_name = os.getenv('USERNAME') if platform == 'win32' else os.getenv('USER')
 
 def blank_current_readline():
-
-    (rows,cols) = struct.unpack('hh', fcntl.ioctl(stdout, termios.TIOCGWINSZ,'1234'))
 
     text_len = len(PINK+my_name+': '+ENDC+readline.get_line_buffer())
     
     # ANSI escape sequences (All VT100 except ESC[0G)
     stdout.write('\x1b[2K')                         # Clear current line
-    stdout.write('\x1b[1A\x1b[2K'*(text_len/cols))  # Move cursor up and clear line
     stdout.write('\x1b[0G')
 
 def c_send(a):
     while(True):
         input = raw_input(PINK+my_name+': '+ENDC)
-        if (input[0] == '/' and len(input)>1):
-            exec_commands(input[1:], a)
-        else:
-            a.send(YELLOW+my_name+': '+ENDC+input)
+        if (input):
+            if (input[0] == '/' and len(input)>1):
+                exec_commands(input[1:], a)
+            else:
+                a.send(YELLOW+my_name+': '+ENDC+input)
 
 def c_recv(a):
     while(True):
         string = a.recv(9999)
-        if string:
+        if string and not shell_active:
             blank_current_readline()
             print string
             saveme = readline.get_line_buffer().split('\n')
 #            if (saveme[-1]):
             stdout.write(PINK+my_name+': '+ENDC+saveme[-1])
             stdout.flush()
+        elif string: # message received while shell is active
+            print GREEN+'('+string+GREEN+')'+ENDC
+        else:
+            blank_current_readline()
+            print GREEN+'someone disconnected'+ENDC
+            exit()
 
 def exec_commands(command, a):
     if (command[0:4] == 'nick'):
@@ -60,8 +64,26 @@ def exec_commands(command, a):
         exit()
     elif (command[0:5] == 'clear'):
         os.system([ 'clear', 'cls' ][ os.name == 'nt' ])
+    elif (command[0:5] == 'blink'):
+        a.send(YELLOW+my_name+': '+ENDC+BLINK+command[6:]+ENDC)
+    elif (command[0:5] == 'shell'):
+        shell = os.getenv('SHELL')
+        if not shell:
+            print GREEN+"you have no default shell"+ENDC
+        else:
+            global shell_active
+            a.send(YELLOW+my_name+GREEN+' started a shell'+ENDC)
+            print GREEN+"you've started a shell"+ENDC
+            shell_active = True
+            os.system(shell)
+            shell_active = False
+            print GREEN+"shell terminated"+ENDC
+            a.send(YELLOW+my_name+GREEN+' terminated their shell'+ENDC)
     elif (command[0:4] == 'exec'):
         os.system(command[5:])
+    elif (command[0:2] == 'me'):
+        a.send(YELLOW+my_name+command[2:]+ENDC)
+        print PINK+my_name+GREEN+command[2:]+ENDC
     else:
         print GREEN+'/'+command.split()[0]+' isn\'t a valid command.'
 
@@ -94,7 +116,8 @@ try:
     value = s.recv(9999)
     s.send(my_name)
     filler = "s are"
-    if (int(value) == 1):
+    clients = int(value)+1
+    if (clients == 1):
         filler = " is"
     print GREEN+ value + " user"+filler+" chatting"+ENDC
     communicate(s);
@@ -102,14 +125,16 @@ try:
 except:
     server = socket(AF_INET, SOCK_STREAM)
     print YELLOW+"nobody's here yet..."+ENDC
-    
+
     server.bind(('', int(argv[2])))
     server.listen(0)
     conn, addr = server.accept()
+    clients += 1
     conn.send(str(clients-1))
     print GREEN+conn.recv(9999)+" connected"+ENDC
     communicate(conn);
 
         
 while(loop):
+    time.sleep(100000)
     pass
